37755ce66c7bdd92240b0b6271a18cc676e10567
Abiodun Busari <hafsahbusari69@gmail.com>
2024-10-15 11:57:41 +0100

    feat: Location map added

--- app/page.tsx
+++ app/page.tsx
@@ -22,6 +22,17 @@ export default function Home() {
           className="w-full"
         />
 
+        {/* map */}
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.4826840290193!2d3.3430708105118194!3d6.586764793379338!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b92213a1785c1%3A0xfbca096bb8cb53fc!2sUnique%20Shopping%20Mall!5e0!3m2!1sen!2sng!4v1686741477397!5m2!1sen!2sng"
+          width="600"
+          height="450"
+          className="w-full rounded-lg"
+          style={{ border: "0" }}
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+
         {/* emergency  */}
         <EmergencyContact />
       </div>
