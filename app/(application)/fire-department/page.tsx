585eb8a6cec3adbc3b4b16170938846b11f99020
Abiodun Busari <hafsahbusari69@gmail.com>
2024-10-15 11:53:56 +0100

    feat: other pages contents

--- app/(application)/fire-department/page.tsx
+++ app/(application)/fire-department/page.tsx
@@ -1,21 +1,22 @@
-import police from "@/public/assets/police-service.png";
+import EmergencyContact from "@/app/components/EmergencyContact";
 import Image from "next/image";
-import EmergencyContact from "./components/EmergencyContact";
+import React from "react";
+import fire from "@/public/assets/fire-service.png";
 
-export default function Home() {
+const FireDepartment = () => {
   return (
     <section className=" py-10 flex flex-col gap-10">
       <div className="flex flex-col gap-3 px-14">
         <h3 className="text-secondary text-semibold-18">Good morning,</h3>
         <p className="text-semibold-32 text-secondary">
-          Find the Nearest Police Station.........
+          Find the Nearest Fire fighting Facility.........
         </p>
       </div>
 
       <div className="flex flex-col gap-8 p-8 ">
         {/* police */}
         <Image
-          src={police}
+          src={fire}
           width={722}
           height={400}
           alt="police image"
@@ -27,4 +28,6 @@ export default function Home() {
       </div>
     </section>
   );
-}
+};
+
+export default FireDepartment;
